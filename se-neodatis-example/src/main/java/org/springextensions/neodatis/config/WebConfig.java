12bac253c562994528c06bfa6b1249067034cc71
jbellmann <joerg.bellmann@googlemail.com>
2012-02-06 23:23:18 +0100

    make thymeleaf works

--- se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
+++ se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
@@ -2,11 +2,16 @@ package org.springextensions.neodatis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateMode;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
@@ -17,11 +22,45 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    @Configuration
+    @Profile("view.jsp")
+    static class Jsp {
+        @Bean
+        public ViewResolver viewResolver() {
+            InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+            viewResolver.setPrefix("/WEB-INF/views/");
+            viewResolver.setSuffix(".jsp");
+            return viewResolver;
+        }
     }
+
+    @Configuration
+    @Profile("view.thymeleaf")
+    static class Thymeleaf {
+        @Bean
+        public SpringTemplateEngine templateEngine() {
+            SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+            springTemplateEngine.setTemplateResolver(templateResolver());
+            return springTemplateEngine;
+        }
+
+        @Bean
+        public ServletContextTemplateResolver templateResolver() {
+            ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+            templateResolver.setPrefix("/WEB-INF/templates/");
+            templateResolver.setSuffix(".html");
+            templateResolver.setTemplateMode(TemplateMode.HTML5);
+            return templateResolver;
+        }
+
+        @Bean
+        public ThymeleafViewResolver thymeleafViewResolver() {
+            ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+            thymeleafViewResolver.setTemplateEngine(templateEngine());
+            //            thymeleafViewResolver.setOrder(1);
+            //            thymeleafViewResolver.setViewNames(new String[] { "*.html", "*.xhtml" });
+            return thymeleafViewResolver;
+        }
+    }
+
 }
