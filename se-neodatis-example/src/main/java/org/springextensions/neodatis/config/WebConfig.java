2f4051d7afba2961fad637c051ea9ecadbd613f9
Joerg Bellmann <joerg.bellmann@googlemail.com>
2012-02-07 01:27:09 +0100

    use messagessources

--- se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
+++ se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
@@ -1,8 +1,12 @@
 package org.springextensions.neodatis.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -17,9 +21,25 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    /**
+     * Messages to support internationalization/localization.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/messages/messages");
+        if (environment.acceptsProfiles("embedded")) {
+            messageSource.setCacheSeconds(0);
+        }
+        return messageSource;
     }
 
     @Configuration
