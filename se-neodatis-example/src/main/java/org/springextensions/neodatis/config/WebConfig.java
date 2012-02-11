ce4a847fc65d01ccfbf792b43b141d3608da3db4
Joerg Bellmann <joerg.bellmann@googlemail.com>
2012-02-11 14:09:47 +0100

    remove jsp, thymeleaf updated to latest 2.0.0, updated WebConfig

--- se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
+++ se-neodatis-example/src/main/java/org/springextensions/neodatis/config/WebConfig.java
@@ -4,19 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.TemplateMode;
 import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
@@ -56,45 +53,29 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return messageSource;
     }
 
-    @Configuration
-    @Profile("view.jsp")
-    static class Jsp {
-        @Bean
-        public ViewResolver viewResolver() {
-            InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-            viewResolver.setPrefix("/WEB-INF/views/");
-            viewResolver.setSuffix(".jsp");
-            return viewResolver;
-        }
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        return springTemplateEngine;
     }
 
-    @Configuration
-    @Profile("view.thymeleaf")
-    static class Thymeleaf {
-        @Bean
-        public SpringTemplateEngine templateEngine() {
-            SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-            springTemplateEngine.setTemplateResolver(templateResolver());
-            return springTemplateEngine;
-        }
+    @Bean
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
+        return templateResolver;
+    }
 
-        @Bean
-        public ServletContextTemplateResolver templateResolver() {
-            ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-            templateResolver.setPrefix("/WEB-INF/templates/");
-            templateResolver.setSuffix(".html");
-            templateResolver.setTemplateMode(TemplateMode.HTML5);
-            return templateResolver;
-        }
-
-        @Bean
-        public ThymeleafViewResolver thymeleafViewResolver() {
-            ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
-            thymeleafViewResolver.setTemplateEngine(templateEngine());
-            //            thymeleafViewResolver.setOrder(1);
-            //            thymeleafViewResolver.setViewNames(new String[] { "*.html", "*.xhtml" });
-            return thymeleafViewResolver;
-        }
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine());
+        //            thymeleafViewResolver.setOrder(1);
+        //            thymeleafViewResolver.setViewNames(new String[] { "*.html", "*.xhtml" });
+        return thymeleafViewResolver;
     }
 
 }
